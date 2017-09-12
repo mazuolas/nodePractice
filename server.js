a5ce956d239c1210ce8521c8257801b69736a87a
Mark Azuolas <markazuolas@gmail.com>
2017-09-12 15:36:20 -0700

    req url displayed

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var time = require('./clock.js')
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html'});
-  res.write(time.myDateTime())
-  res.end('Hello World!')
+  res.write(time.myDateTime());
+  res.write(req.url);
+  res.end();
+
 }).listen(8080);
