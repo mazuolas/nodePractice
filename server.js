dd5935600957ffc69b8f75acb21176457e77cea1
Mark Azuolas <markazuolas@gmail.com>
2017-09-12 16:41:42 -0700

    add logs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require('http');
 var time = require('./clock.js')
 var url = require('url')
-
+console.log('Server started');
 http.createServer(function (req, res) {
+  console.log('Recieved request');
+  console.log(req.url);
   res.writeHead(200, { 'Content-Type': 'text/html'});
   res.write(time.myDateTime());
   var q = url.parse(req.url, true).query
