e0b413e716871dbbded29f2d09218366fe6e6158
Mark Azuolas <markazuolas@gmail.com>
2017-09-12 15:19:33 -0700

    add clock to server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var time = require('./clock.js')
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html'});
+  res.write(time.myDateTime())
   res.end('Hello World!')
 }).listen(8080);
