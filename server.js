5b57f8e77b0ccddccdccbd3eb94c9d221e1cdd0e
Mark Azuolas <markazuolas@gmail.com>
2017-09-12 15:45:30 -0700

    querey strings

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var http = require('http');
 var time = require('./clock.js')
+var url = require('url')
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html'});
   res.write(time.myDateTime());
-  res.write(req.url);
+  var q = url.parse(req.url, true).query
+  res.write(q.test);
   res.end();
 
 }).listen(8080);
